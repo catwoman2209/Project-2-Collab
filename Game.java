8d685b56e8e03532a997d7cd8e7f580e8a5cc037
Christiana Taylor <60984198+catwoman2209@users.noreply.github.com>
2020-10-02 11:14:15 -0500

    added description

--- Game.java
+++ Game.java
@@ -5,8 +5,26 @@
  */
 package cs2365.project.pkg2;
 
+import java.util.*;
+
 /**
- *
+ * Game class requires Deck and Card classes to function
+ * Has 3 methods:
+ *  - start
+ *      Asks user for # of decks, shuffle preference, and whether to remove
+ *      Special cards or not
+ *  - formatCard
+ *      Counts amount of reps per workout, takes into account special cards
+ *      and performs functions as listed in Project 2 PDF
+ *  - getMax
+ *      For statistics collection
+ * 
+ * start
+ * draws 7 cards
+ * sends to formatCard
+ * output displayed per exercise on console and in HTML
+ * repeat until deck is depleted
+ * 
  * @author chris
  */
 public class Game {
