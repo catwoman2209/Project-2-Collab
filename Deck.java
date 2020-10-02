eca3323e9e7d59c4c0777cf402c9a08583ed27ff
Christiana Taylor <60984198+catwoman2209@users.noreply.github.com>
2020-10-02 11:02:53 -0500

    Added description

--- Deck.java
+++ Deck.java
@@ -5,7 +5,16 @@
  */
 package cs2365.project.pkg2;
 
+import java.util.*;
+
+
 /**
+ * Deck class creates 108 UNO cards and stores them in an array list
+ * Functions:
+ *  -Shuffle
+ *  -Check if empty
+ *  -Draws hands for user
+ * Statistics collection functions will store data for each deck
  *
  * @author chris
  */
