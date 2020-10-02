7c948ae6d9032c1b69c283c6c53d01ed3f407272
Christiana Taylor <60984198+catwoman2209@users.noreply.github.com>
2020-10-02 09:45:51 -0500

    Initial upload of class files

---
+++ Deck.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs2365.project.pkg2;
+
+/**
+ *
+ * @author chris
+ */
+public class Deck {
+    
+}
