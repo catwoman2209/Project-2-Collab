f3bf80f6b212633c5c473beb30dafa7df7799249
Christiana Taylor <60984198+catwoman2209@users.noreply.github.com>
2020-10-19 17:52:03 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -28,5 +28,460 @@ import java.util.*;
  * @author chris
  */
 public class Game {
+    //FOR STAT COLLECTION--> 
+        
+    int[] StatsWorkout = new int[3];
+    int[] WorkoutReps = new int[5];
+    int[] skippedR = new int[4];
+
+
+   Output html = new Output();
+       
+   /**
+        Main function that Starts and runs through the game.
+        * Goes through all the decks and changes decks when no more cards can be drawn from it
+   
+    */
+    public void StartGame(int inp1, int inp2, int inp3)
+    {      
+    	
+        if(inp1 > 3)
+        {
+            inp1 = 3;
+        }
+        else if(inp1 <= 0)
+        {
+            inp1 = 1;
+        }
+        
+        //filling the Decks
+        Deck[] Decks = new Deck[inp1];
+        
+       for(int a = 0; a < Decks.length; a++)
+       {
+           Decks[a] = new Deck();
+           Decks[a].create();
+       }
+        
+        //shuffling all the decks together
+        if(inp2 == 1 && Decks.length > 1)
+        {
+            ArrayList<Card> All = new ArrayList<Card>();
+            for(int a = 0; a < Decks.length; a++)
+            {
+                ArrayList<Card> DeckInd = Decks[a].getDeck();
+                for(int b = 0; b < DeckInd.size(); b++)
+                {
+                    All.add(DeckInd.get(b));
+                }
+            }
+            
+            Collections.shuffle(All);
+            
+            //redistrbuting the cards into the decks
+            int index = 0;
+            for(int a = 0; a < Decks.length; a++)
+            {
+                ArrayList<Card> NewD = new ArrayList<Card>();
+                for(int b = 0; b < 108; b++)
+                {
+                    if(All.get(index) != null)
+                    {
+                        NewD.add(All.get(index));
+                        index++;
+                    }
+                }
+                
+                Decks[a].refill(NewD);
+            }
+        }
+        
+        //The Player can then choose to remove special cards!!!
+        if(inp3 == 1)
+        {
+            for(int a = 0; a < Decks.length; a++)
+            {
+                Decks[a].removeSpec();
+            }
+        }
+        
+        //Let the game begin!
+        int count = 1;
+        ArrayList<Card> Hand = new ArrayList<Card>();
+        for(int a = 0; a  < Decks.length; a++)
+        {
+            Hand.clear();
+            while(!Decks[a].isEmpty())
+            {
+                if(Hand.size() == 0)
+                {
+                    ArrayList<Card> Temp = Decks[a].drawCards(7);
+                    
+                    //function to sort the hand and display the data
+                    html.setHandHeading(count);
+                    count++;
+                    SortingHand(Temp);
+
+                    for(int b = 0; b < Temp.size(); b++)
+                    {
+                        Hand.add(Temp.get(b));
+                    }
+                    
+                    //Hand has been filled
+                    format(Hand, Decks[a]);
+                    
+                    //Will draw a new hand
+                    Temp.clear();
+                    Hand.clear();
+                    
+                }
+                
+                //Number of Cards Left3
+                
+            int RemainingCards = Decks[a].onDeck() + (108 * ( Decks.length - (a+1)));
+            html.setCardsLeft(RemainingCards);
+            System.out.println("Cards left to draw from - " + RemainingCards + "\n");
+                
+            }
+        }
+        
+        //Display Stat Data
+        DisplayStatData(StatsWorkout, WorkoutReps, skippedR);
+        html.writeToFile();
+        System.out.println("Game over!!! Hopefully you had a good workout!");
+    }
     
+    /**
+     * Function takes in each hand and the Deck that you are currently on and formats the series
+     * of cards into a workout routine via card color, value, and special type.
+     * @param H
+     * @param D
+     */
+    void format(ArrayList<Card> H, Deck D)
+    {
+        int RepsSitUps = 0;
+        int RepsPushUps = 0;
+        int RepsSquat = 0;
+        int RepsLounges = 0;
+        int Burpees = 0;
+        
+        
+        for(int a = 0; a < H.size(); a++)
+        {
+            boolean isS = H.get(a).isSpec;
+            int V = H.get(a).getVal();
+            String Col = H.get(a).getType(true);
+            String Spe = H.get(a).getType(false);
+            
+            if(!isS)
+            {
+                if(Col.equals(("Red")))
+                {
+                    RepsSitUps += V;
+                }
+                else if(Col.equals(("Blue")))
+                {
+                    RepsPushUps += V;
+                }
+                else if(Col.equals(("Yellow")))
+                {
+                    RepsSquat += V;
+                }
+                else
+                {
+                    //Green
+                    RepsLounges += V;
+                }
+            }
+        }
+        
+        
+        for(int a = 0; a < H.size(); a++)
+        {
+            boolean isS = H.get(a).isSpec;
+            int V = H.get(a).getVal();
+            String Col = H.get(a).getType(true);
+            String Spe = H.get(a).getType(false);
+            
+            if(isS)
+            {
+                if(Spe.equals("Skip"))
+                {
+                    if(Col.equals(("Red")))
+                    {
+                        skippedR[0] += RepsSitUps;
+                        RepsSitUps = 0;
+                    }
+                    else if(Col.equals(("Blue")))
+                    {
+                        skippedR[1] += RepsPushUps;
+                        RepsPushUps =0;
+                    }
+                    else if(Col.equals(("Yellow")))
+                    {
+                        skippedR[2] += RepsSquat;
+                        RepsSquat =0;
+                    }
+                    else
+                    {
+                        //Green
+                        skippedR[3] += RepsLounges;
+                        RepsLounges = 0;
+                    }  
+                }
+                else if(Spe.equals("Draw 2"))
+                {
+                    if(Col.equals(("Red")))
+                    {
+                        RepsSitUps *= 2;
+                    }
+                    else if(Col.equals(("Blue")))
+                    {
+                        RepsPushUps *= 2;
+                    }
+                    else if(Col.equals(("Yellow")))
+                    {
+                        RepsSquat *= 2;
+                    }
+                    else
+                    {
+                        //Green
+                        RepsLounges *= 2;
+                    }                     
+                }
+                else if(Spe.equals(("Wild")) || Spe.equals(("Wild Draw 4")))
+                {
+                    Burpees += 10;
+                    RepsSitUps *= 4;
+                    RepsPushUps *= 4;
+                    RepsSquat *= 4;
+                    RepsLounges *= 4;
+                }
+            }
+        }
+        
+        
+        if(RepsSitUps > 0)
+        {
+            System.out.println("Do " + RepsSitUps + " reps of Sit Ups!");
+        }
+        
+        if(RepsPushUps > 0)
+        {
+            System.out.println("Do " + RepsPushUps + " reps of Push Ups!");
+        }
+        
+        if(RepsSquat > 0)
+        {
+            System.out.println("Do " + RepsSquat + " reps of Squats!");
+        }
+        
+        if(RepsLounges > 0)
+        {
+            System.out.println("Do " + RepsLounges + " reps of Lunges!");
+        }
+        
+        if(Burpees > 0)
+        {
+            System.out.println("Do " + Burpees + " reps of Burpees!");
+        }
+        
+        System.out.println();
+        
+        //Stat collection function gets called
+        WorkoutReps[0] += RepsPushUps;
+        WorkoutReps[1] += RepsSitUps;
+        WorkoutReps[2] += RepsSquat;
+        WorkoutReps[3] += RepsLounges;
+        WorkoutReps[4] += Burpees;
+        int TotalR = RepsPushUps + RepsSitUps + RepsSquat + RepsLounges + Burpees;
+        int MaxR = GetMaxReps(RepsPushUps, RepsSitUps, RepsSquat, RepsLounges, Burpees);
+        
+        D.stats(TotalR, 0, MaxR);
+        
+        updateStats(D.statData());
+        
+    }
+    
+     /**
+     * Function globally keeps track of every Rep performed, every skipped rep, and the max reps
+     * performed in a single set throughout the entire game
+     * @param Stats
+     */
+    void updateStats(int[] Stats)
+    {
+        StatsWorkout[0] += Stats[0];
+        StatsWorkout[1] += Stats[1];
+        
+        if(StatsWorkout[2] < Stats[2])
+        {
+            StatsWorkout[2] = Stats[2];
+        }
+        
+    }
+    
+     /**
+     * Function displays the stats of game, called at the end of the game. Takes in two
+     * Arrays that both have data pertaining to total stats
+     * @param TotalStats
+     * @param IndStats
+     */
+    void DisplayStatData(int[] TotalStats, int[] IndStats, int[] skipped)
+    {
+    	html.setExerciseHeading();
+        System.out.println("---Workout Stats---");
+        
+        System.out.println("Total Reps - " + TotalStats[0]);
+        System.out.println("Total Skipped Reps - " + TotalStats[1]);
+        System.out.println("Max Reps performed - " + TotalStats[2] + "\n");
+        
+        System.out.println("Total Pushups - " + IndStats[0]);
+        html.setExerciseTotal(IndStats[0], skipped[0], "Pushups");
+        System.out.println("Total Situps - " + IndStats[1]);
+        html.setExerciseTotal(IndStats[1], skipped[1], "Situps");
+        System.out.println("Total Squats - " + IndStats[2]);
+        html.setExerciseTotal(IndStats[2], skipped[2], "Squats");
+        System.out.println("Total Lunges - " + IndStats[3]);
+        html.setExerciseTotal(IndStats[3], skipped[3], "Lunges");
+        System.out.println("Total Burpees - " + IndStats[4] + "\n");
+        html.setExerciseTotal(IndStats[4], 0 , "Burpees");
+    }
+            
+      /**
+     * Sorting method that groups a set of cards from hand H, into separate arrayList via color
+     * @param H
+     */
+    void SortingHand(ArrayList<Card> H)
+    {
+        ArrayList<Card> Wild = new ArrayList<Card>();
+        ArrayList<Card> Red = new ArrayList<Card>();
+        ArrayList<Card> Blue = new ArrayList<Card>();
+        ArrayList<Card> Yellow = new ArrayList<Card>();
+        ArrayList<Card> Green = new ArrayList<Card>();
+        
+        for(int a = 0; a < H.size(); a++)
+        {
+            String C = H.get(a).getType(true);
+            
+            if(C.equals("Red"))
+            {
+                Red.add(H.get(a));
+            }
+            else if(C.equals("Blue"))
+            {
+                Blue.add(H.get(a));
+            }
+            else if(C.equals("Yellow"))
+            {
+                Yellow.add(H.get(a));
+            }
+            else if(C.equals("Green"))
+            {
+                Green.add(H.get(a));
+            }
+            else
+            {
+                Wild.add(H.get(a));
+            }
+
+        }
+        
+        //sort them
+        
+        Red = SortByRank(Red);
+        Blue = SortByRank(Blue);
+        Yellow = SortByRank(Yellow);
+        Green = SortByRank(Green);
+        
+        System.out.println("-----New Hand-----");
+        
+        for(Card red1: Red)
+        {
+        	html.setCardDescription(red1);
+            System.out.print(red1.cardOut() + ", ");
+        }
+        for(Card blue1: Blue)
+        {
+        	html.setCardDescription(blue1);
+            System.out.print(blue1.cardOut() + ", ");
+        }
+        for(Card yellow1: Yellow)
+        {
+        	html.setCardDescription(yellow1);
+            System.out.print(yellow1.cardOut() + ", ");
+        }
+        for(Card green1: Green)
+        {
+        	html.setCardDescription(green1);
+            System.out.print(green1.cardOut() + ", ");
+        }
+        for(Card wild1: Wild) {
+        	html.setCardDescription(wild1);
+            System.out.print(wild1.cardOut() + ", ");
+        }
+        
+        System.out.println();
+        
+    }
+    
+     /**
+     * Sorting method that takes in a set of cards, already sorted by color, and sorts them via value that they hold
+     * and returns the sorted list from smallest to largest.
+     * @param HandCol
+     * @return
+     */
+    ArrayList<Card> SortByRank(ArrayList<Card> HandCol)
+    {
+        for(int j = 0; j < HandCol.size()-1; j++)
+        {
+		for (int i = 0; i < HandCol.size()-1; i++)
+        	{
+            		if (HandCol.get(i).getVal() > HandCol.get(i+1).getVal())
+            		{
+                		Card PlaceHolder = HandCol.get(i+1);
+                		HandCol.set(i+1, HandCol.get(i));
+               			HandCol.set(i, PlaceHolder);
+            		}
+        	}
+        }
+        
+        return HandCol;
+    }
+    
+    /**
+     * Function takes in all the reps done per exercise in a given hand, and finds which one
+     * was done the most. Essentially, finds the largest value passed in.
+     * @param Push
+     * @param Sit
+     * @param L
+     * @param Sq
+     * @param B
+     * @return
+     */
+    int GetMaxReps(int Push, int Sit, int L, int Sq, int B)
+    {
+        
+        if(Push > Sit && Push > L && Push > Sq && Push > B)
+        {
+        	html.setMaxHandReps(Push, "PushUps");
+            return Push;
+        }
+        else if(Sit > Push && Sit > L && Sit > Sq && Sit > B)
+        {
+        	html.setMaxHandReps(Sit, "Situps");
+            return Sit;
+        }
+        else if(L > Push && L > Sit && L > Sq && L > B)
+        {
+        	html.setMaxHandReps(L, "Lunges");
+            return L;
+        }
+        else if( Sq > Push && Sq > Sit && Sq > L && Sq > B)
+        {
+        	html.setMaxHandReps(Sq, "Squats");
+            return Sq;
+        }
+        html.setMaxHandReps(B, "Burpees");
+        return B;
+    }
 }
